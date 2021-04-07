92b54db94554a570c20db157865e164fe043f3b3
Nathan Silva <braganathan38@gmail.com>
2021-04-07 13:27:42 -0300

    Initial commit

---
+++ src/main/java/academy/devdojo/springboot2/requests/AnimePutRequestBody.java
@@ -0,0 +1,12 @@
+package academy.devdojo.springboot2.requests;
+
+import lombok.Data;
+
+@Data
+public class AnimePutRequestBody {
+
+    private Long id;
+    private String name;
+}
+
+
---
+++ src/test/java/academy/devdojo/springboot2/repository/AnimeRepositoryTest.java
@@ -0,0 +1,98 @@
+package academy.devdojo.springboot2.repository;
+
+import academy.devdojo.springboot2.domain.Anime;
+import academy.devdojo.springboot2.util.AnimeCreator;
+import lombok.extern.log4j.Log4j2;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import javax.validation.ConstraintViolationException;
+import java.util.List;
+import java.util.Optional;
+
+@DataJpaTest
+@DisplayName("Tests for anime Repository")
+@Log4j2
+class AnimeRepositoryTest {
+
+    @Autowired
+    private AnimeRepository animeRepository;
+
+    @Test
+    @DisplayName("Save persists anime when Successful")
+    void save_PersistAnime_WhenSeccessful(){
+        Anime animeToBeSaved = AnimeCreator.createAnimeToBeSaved();
+        Anime animeSaved = this.animeRepository.save(animeToBeSaved);
+
+        Assertions.assertThat(animeSaved).isNotNull();
+        Assertions.assertThat(animeSaved.getId()).isNotNull();
+        Assertions.assertThat(animeSaved.getName()).isEqualTo(animeToBeSaved.getName());
+    }
+
+    @Test
+    @DisplayName("Save updates anime when Successful")
+    void save_UpdateAnime_WhenSeccessful(){
+        Anime animeToBeSaved = AnimeCreator.createAnimeToBeSaved();
+        Anime animeSaved = this.animeRepository.save(animeToBeSaved);
+        animeSaved.setName("Overlord");
+        Anime animeUpdated = this.animeRepository.save(animeSaved);
+        log.info(animeUpdated.getName());
+
+        Assertions.assertThat(animeUpdated).isNotNull();
+        Assertions.assertThat(animeUpdated.getId()).isNotNull();
+        Assertions.assertThat(animeUpdated.getName()).isEqualTo(animeSaved.getName());
+    }
+
+    @Test
+    @DisplayName("Delete removes anime when Successful")
+    void delete_Removes_Anime_WhenSeccessful(){
+        Anime animeToBeSaved = AnimeCreator.createAnimeToBeSaved();
+        Anime animeSaved = this.animeRepository.save(animeToBeSaved);
+        this.animeRepository.delete(animeSaved);
+
+        Optional<Anime> animeOptional = this.animeRepository.findById(animeSaved.getId());
+
+        Assertions.assertThat(animeOptional).isEmpty();
+    }
+
+    @Test
+    @DisplayName("Find By Name returns list of anime when Successful")
+    void findByName_RetunrsListOfAnime_WhenSeccessful(){
+        Anime animeToBeSaved = AnimeCreator.createAnimeToBeSaved();
+        Anime animeSaved = this.animeRepository.save(animeToBeSaved);
+
+        String name = animeSaved.getName();
+
+        List<Anime> animes = this.animeRepository.findByName(name);
+
+        Assertions.assertThat(animes).isNotEmpty();
+        Assertions.assertThat(animes).contains(animeSaved);
+    }
+
+    @Test
+    @DisplayName("Find By Name returns empty list of anime when no anime is found")
+    void findByName_RetunrsEmptyList_WhenAnimeIsNotFound(){
+        List<Anime> animes = this.animeRepository.findByName("xaxa");
+
+        Assertions.assertThat(animes).isEmpty();
+    }
+
+    @Test
+    @DisplayName("Save throw ConstraintViolationException when name is empty")
+    void save_ThrowsConstraintViolationException_WhenNameIsEmpty(){
+        Anime anime = new Anime();
+//        Assertions.assertThatThrownBy(() -> this.animeRepository.save(anime))
+//                .isInstanceOf(ConstraintViolationException.class);
+
+        Assertions.assertThatExceptionOfType(ConstraintViolationException.class)
+                .isThrownBy(() -> this.animeRepository.save(anime))
+                .withMessageContaining("The anime name cannot be empty");
+    }
+
+
+
+
+}
